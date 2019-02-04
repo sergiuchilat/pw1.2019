c32787eba11e34a91836d056c24168ad35e75850
usarb <chilatsergiu@gmail.com>
2019-02-04 14:27:34 +0200

    Lectia 2. Programare obiect orientata

---
+++ 2.3.1/js/main.js
@@ -0,0 +1,21 @@
+class Car{
+
+	refill(){
+		console.log('enter to fuel station');
+		console.log('stop engine');
+		console.log('push out passengers');
+		console.log('refill');
+		console.log('start engine');
+		console.log('leave fuel station');
+	}
+	move(){
+		console.log('Move');
+	}
+}
+
+/**/
+
+let mercedes = new Car();
+mercedes.refill();
+mercedes.move();
+
