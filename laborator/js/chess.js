f34a48e641296f7c0891441e0432d2d61364a586
usarb <chilatsergiu@gmail.com>
2019-02-11 12:56:29 +0200

    Laborator: tabla de sah. Desenarea 8 linii

--- laborator/js/chess.js
+++ laborator/js/chess.js
@@ -1,16 +1,27 @@
 window.onload = function () {
-	addLine();
+	drawTable();
 }
 
-function addLine(){
-	for (let i = 1; i<= 8; i++) {
+function drawTable(){
+	for (let line = 1; line <= 8; line++) {
+		addLine(line);
+	}
+}
+
+function addLine(line){
+	let whiteColor = true;
+	if(line % 2 === 0){
+		whiteColor = false;
+	}
+	for (let i = 1; i <= 8; i++) {
 		let newPosition = document.createElement('div');
-		if(i % 2 !== 0){
-			newPosition.classList.add('black-position');
-		}else{
+		if(whiteColor){
 			newPosition.classList.add('white-position');
+		}else{
+			newPosition.classList.add('black-position');
 		}
 		newPosition.classList.add('position');
 		document.getElementById('table').appendChild(newPosition);
+		whiteColor = !whiteColor;
 	}	
 }
